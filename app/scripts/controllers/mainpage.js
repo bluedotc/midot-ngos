5b9f77a43ab505ab3008bc740038776a9e0946d5
Adam Kariv <adam.kariv@gmail.com>
2016-01-20 16:46:00 +0200

    Detail view

--- app/scripts/controllers/mainpage.js
+++ app/scripts/controllers/mainpage.js
@@ -10,7 +10,7 @@
 angular.module('midotApp')
   .controller('MainpageCtrl', function (rows) {
     var that = this;
-
+    this.selectedRow = null;
     this.rows = [];
     rows.then(function(data) {
       that.rows = data[0];
@@ -18,4 +18,25 @@ angular.module('midotApp')
         _.union(_.map(that.rows, function(d) { return d.operation_field; }),
                 _.map(that.rows, function(d) { return d.operation_field_2; }));
     });
+
+    this.onHover = function(row,event) {
+      that.selectedRow = row;
+      if ( row !== null ) {
+        var el = $(event.currentTarget);
+        var width = el.width();
+        var height = el.height();
+        //var next = $(event.currentTarget.nextElementSibling);
+        var panel = $("#detail-card");
+        var pos = el.offset();
+        panel.css('width',width+'px');
+        if (pos) {
+          console.log(pos);
+          panel.css('position','absolute');
+          panel.css('top',(pos.top+22+height)+'px');
+          panel.css('right',pos.left+'px');
+        } else {
+          panel.css('position','static');
+        }
+      }
+    };
   });
